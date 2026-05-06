af143280647f4b8439bea25b654b8815f533c6a1
Suzan Chandra <suzanchandra@Suzans-Mac-mini.local>
2026-05-07 01:03:04 +0600

    problem 3 solved

--- solutions.ts
+++ solutions.ts
@@ -1,14 +1,28 @@
-//* Problem 1 */
+//* Problem 1 solution */
 
 function filterEvenNumbers(numbers: number[]): number[] {
   return numbers.filter((num) => num % 2 === 0);
 }
-console.log(filterEvenNumbers([1, 2, 3, 4, 5, 6]));
 
-//* Problem 2 */
+//* Problem 2 solution */
 
 function reverseString(input: string): string {
   return input.split("").reverse().join("");
 }
 
-console.log(reverseString("typescript"));
+//* Problem 3 solution */
+
+type StringOrNumber = string | number;
+
+function checkType(value: StringOrNumber): string {
+  if (typeof value === "string") {
+    return "String";
+  } else if (typeof value === "number") {
+    return "Number";
+  } else {
+    return "Please give me only string or number";
+  }
+}
+
+console.log(checkType("Hello"));
+console.log(checkType(42));
