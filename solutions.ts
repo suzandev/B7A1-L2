735727ef093a0c6c4cd01eb2c6f2365cb4eea193
Suzan Chandra <suzanchandra@Suzans-Mac-mini.local>
2026-05-07 00:53:15 +0600

    problem 2 solved

--- solutions.ts
+++ solutions.ts
@@ -3,5 +3,12 @@
 function filterEvenNumbers(numbers: number[]): number[] {
   return numbers.filter((num) => num % 2 === 0);
 }
-
 console.log(filterEvenNumbers([1, 2, 3, 4, 5, 6]));
+
+//* Problem 2 */
+
+function reverseString(input: string): string {
+  return input.split("").reverse().join("");
+}
+
+console.log(reverseString("typescript"));
